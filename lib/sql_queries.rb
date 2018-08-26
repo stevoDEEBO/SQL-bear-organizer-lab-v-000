f4108bf80c4bfcafad9fd983058767ecb100a049
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-08-26 03:58:43 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,11 +19,11 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT max(color) count(color);"
+  "SELECT max(color) from bears count(color);"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT count(temperament) where temperament = 'goofy';"
+  "SELECT count(temperament) from bears where temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
