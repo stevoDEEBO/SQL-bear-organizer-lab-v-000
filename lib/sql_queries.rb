8ddbcfa76a7f2a3defd7797dd6f97ba45668fc7c
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-08-26 04:04:38 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name,age from bears order by age desc limit 1;"
+  "SELECT name,age from bears order by age limit 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
