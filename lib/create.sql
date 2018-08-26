589aa0c6c55eabc55cab5361f80c7431399cc3c7
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-08-26 03:14:24 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,7 +2,7 @@ CREATE TABLE bears (
   id INTEGER PRIMARY KEY,
       name TEXT,
       age INTEGER,
-      gpa FLOAT,
+      gender char(1)(The choices would be "M" or "F"),
       color TEXT,
       temperament TEXT,
       alive BOOLEAN
