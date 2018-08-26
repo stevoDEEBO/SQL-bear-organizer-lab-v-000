b1a65ea0043d5d1c734a46eb614d06062b20d319
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-08-26 03:30:33 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -3,7 +3,7 @@ def selects_all_female_bears_return_name_and_age
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "select name from bears order by name"
+  "select name from bears ORDER BY name"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
